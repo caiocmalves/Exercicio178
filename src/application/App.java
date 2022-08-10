04802f5497da6156b9518df60d4b808de89b9a19
Caio Carvalho <caio.cmalves@gmail.com>
2022-08-10 18:52:49 -0300

    resposta final  da aula 178

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,46 @@
+package application;
+
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.WithdrawException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        try {
+            System.out.println("Enter account data");
+            System.out.print("Number: ");
+            int number = sc.nextInt();
+            System.out.print("holder: ");
+            String holder = sc.next();
+            System.out.print("Initial balance: ");
+            double balance = sc.nextDouble();
+            System.out.print("Withdraw limit: "); 
+            double withdrawLimit = sc.nextDouble();
+            System.out.println();
+            Account c = new Account(number, holder, balance, withdrawLimit);
+            System.out.println();
+
+            System.out.print("Enter amount for withdraw: ");
+            double amount = sc.nextDouble();
+            c.withdraw(amount);
+
+            System.out.println("New balance: " + c.getBalance());
+
+
+
+
+        }
+        catch (WithdrawException e) {
+            System.out.println(e.getMessage());
+        }
+        catch (RuntimeException e) {
+            System.out.println("Unexpected error.");
+        }
+
+        sc.close();
+
     }
 }
---
+++ src/model/exceptions/WithdrawException.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class WithdrawException extends RuntimeException {
+    
+    public WithdrawException(String msg) {
+        super(msg);
+    }
+    
+}
